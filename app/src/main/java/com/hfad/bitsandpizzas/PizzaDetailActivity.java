fc1c7b49a5e69e316b38b7f5ac772dc3e96b97cd
andreynho2006 <andreynho2006@gmail.com>
2018-12-13 17:11:16 +0200

    add a new activity to display individuals items

---
+++ app/src/main/java/com/hfad/bitsandpizzas/PizzaDetailActivity.java
@@ -0,0 +1,13 @@
+package com.hfad.bitsandpizzas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PizzaDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pizza_detail);
+    }
+}
