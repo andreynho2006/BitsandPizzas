9240fe580ad3a4852aa43b8e201b78b2d651cb8c
andreynho2006 <andreynho2006@gmail.com>
2018-12-13 16:27:43 +0200

    make fragment_pasta and, Pasta class and Pastafragment

--- app/src/main/java/com/hfad/bitsandpizzas/PastaFragment.java
+++ app/src/main/java/com/hfad/bitsandpizzas/PastaFragment.java
@@ -1,19 +1,31 @@
 package com.hfad.bitsandpizzas;
 import android.os.Bundle;
-import android.support.v4.app.ListFragment;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-public class PastaFragment extends ListFragment {
+public class PastaFragment extends Fragment {
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(
-                inflater.getContext(),
-                android.R.layout.simple_list_item_1,
-                getResources().getStringArray(R.array.pasta));
-        setListAdapter(adapter);
-        return super.onCreateView(inflater, container, savedInstanceState);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        RecyclerView pastaRecycler = (RecyclerView)inflater.inflate(R.layout.fragment_pasta, container, false);
+        String[] pastaNames = new String[Pasta.pastas.length];
+        for (int i = 0; i < pastaNames.length; i++) {
+            pastaNames[i] = Pasta.pastas[i].getName();
+        }
+
+        int[] pastaImages = new int[Pasta.pastas.length];
+        for (int i = 0; i < pastaImages.length; i++) {
+            pastaImages[i] = Pasta.pastas[i].getImageResourceId();
+        }
+
+        CaptionedImagesAdapter adapter = new CaptionedImagesAdapter(pastaNames, pastaImages);
+
+        GridLayoutManager layoutManager = new GridLayoutManager(getActivity(), 2);
+        pastaRecycler.setLayoutManager(layoutManager);
+        return pastaRecycler;
+
     }
 }
